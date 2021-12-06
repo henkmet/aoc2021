122017fb47d7e54f53e7870d1d65ef8614c4051b
henkmet <henkmetselaar@gmail.com>
2021-12-06 23:48:14 +0800

    Day 6 added. Only one program today

---
+++ 06a.m
@@ -0,0 +1,14 @@
+#!/usr/bin/octave -qf
+fid=fopen('06.txt', 'r');
+data = csvread(fid);
+fclose(fid);
+y=zeros(1,9);
+days=input("Number of days (80/256)"); days
+for i=1:9 y(i)=sum(data==i-1); endfor # octave starts indexing at 1
+for i=1:days
+        ytemp(1:8)=y(2:9);
+        ytemp(7) = ytemp(7)+y(1);
+        ytemp(9) = y(1);
+        y=ytemp;
+endfor
+fprintf('The final number of lanternfish is %d\n', sum(y));
