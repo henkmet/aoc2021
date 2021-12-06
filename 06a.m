7ecd87f8b57dacd1fb31127712bf0fae85fb567d
henkmet <henkmetselaar@gmail.com>
2021-12-06 23:53:10 +0800

    make it nice

--- 06a.m
+++ 06a.m
@@ -3,7 +3,7 @@ fid=fopen('06.txt', 'r');
 data = csvread(fid);
 fclose(fid);
 y=zeros(1,9);
-days=input("Number of days (80/256)"); days
+days=input("Number of days (80/256)");
 for i=1:9 y(i)=sum(data==i-1); endfor # octave starts indexing at 1
 for i=1:days
         ytemp(1:8)=y(2:9);
@@ -11,4 +11,4 @@ for i=1:days
         ytemp(9) = y(1);
         y=ytemp;
 endfor
-fprintf('The final number of lanternfish is %d\n', sum(y));
+fprintf('The final number of lanternfish after %d days is %d\n', days, sum(y));
