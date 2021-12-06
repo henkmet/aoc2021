45af205778885b4e6b8d1c0c8f7b2fa530d55585
henkmet <henkmetselaar@gmail.com>
2021-12-07 00:03:45 +0800

    get rid of temporary variable (nearly)

--- 06a.m
+++ 06a.m
@@ -1,14 +1,16 @@
 #!/usr/bin/octave -qf
+t=cputime();
 fid=fopen('06.txt', 'r');
 data = csvread(fid);
 fclose(fid);
 y=zeros(1,9);
-days=input("Number of days (80/256)");
+days=input("Number of days (80/256) ");
 for i=1:9 y(i)=sum(data==i-1); endfor # octave starts indexing at 1
 for i=1:days
-        ytemp(1:8)=y(2:9);
-        ytemp(7) = ytemp(7)+y(1);
-        ytemp(9) = y(1);
-        y=ytemp;
+        y(10)=y(1);
+        y(1:9) = y(2:10);
+        y(7) = y(7)+y(9);
+        y(10)=[];
 endfor
 fprintf('The final number of lanternfish after %d days is %d\n', days, sum(y));
+fprintf('This took %d seconds\n', cputime()-t);
